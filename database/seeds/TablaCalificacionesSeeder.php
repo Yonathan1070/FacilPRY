966760fcfa3d2c77c39d62a3eda89837217cc72f
Yonathan <ycbohorquez@ucundinamarca.edu.co>
2019-07-05 19:06:40 -0500

    Creacion de los Seeder de cada una de las Tablas de la BD

---
+++ database/seeds/TablaCalificacionesSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class TablaCalificacionesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
---
+++ database/seeds/TablaUsuariosRolesSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class TablaUsuariosRolesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
