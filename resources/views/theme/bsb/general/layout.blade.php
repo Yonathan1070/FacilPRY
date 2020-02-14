be9d7aca61d6ed1b06ae017e173c22e145832377
Yonathan Bohorquez <ycbohorquez@ucundinamarca.edu.co>
2020-02-14 10:31:53 -0500

    Tawk to para chat de asistencia

--- resources/views/theme/bsb/general/layout.blade.php
+++ resources/views/theme/bsb/general/layout.blade.php
@@ -68,7 +68,19 @@
 
     <!-- Demo Js -->
     <script src="{{asset("assets/bsb/js/demo.js")}}"></script>
-
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+        (function(){
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='https://embed.tawk.to/5e3d9b2f298c395d1ce6c749/default';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
+        })();
+    </script>
+    <!--End of Tawk.to Script-->
     @yield('scripts')
 </body>
 
