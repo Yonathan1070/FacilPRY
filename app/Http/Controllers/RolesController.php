1e80b65206054b800a7eb7521b0dd500db3ff7b8
Yonathan Bohorquez <ycbohorquez@ucundinamarca.edu.co>
2020-03-04 21:12:46 -0500

    Código documentado Roles Controller

--- app/Http/Controllers/RolesController.php
+++ app/Http/Controllers/RolesController.php
@@ -10,50 +10,87 @@ use App\Models\Tablas\Roles;
 use App\Models\Tablas\Usuarios;
 use Illuminate\Database\QueryException;
 
+/**
+ * Roles Controller, donde se harán las distintas operaciones sobre la Tabla Roles
+ * 
+ * @author: Yonathan Bohorquez
+ * @email: ycbohorquez@ucundinamarca.edu.co
+ * 
+ * @author: Manuel Bohorquez
+ * @email: jmbohorquez@ucundinamarca.edu.co
+ * 
+ * @version: dd/MM/yyyy 1.0
+ */
 class RolesController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Muestra la vista con el listado de los roles del sistema
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\View\View Vista del listado de roles
      */
     public function index()
     {
         can('listar-roles');
-        $permisos = ['crear'=> can2('crear-roles'), 'editar'=>can2('editar-roles'), 'eliminar'=>can2('eliminar-roles')];
-        $notificaciones = Notificaciones::where('NTF_Para', '=', session()->get('Usuario_Id'))->orderByDesc('created_at')->get();
-        $cantidad = Notificaciones::where('NTF_Para', '=', session()->get('Usuario_Id'))->where('NTF_Estado', '=', 0)->count();
+        $permisos = [
+            'crear'=> can2('crear-roles'),
+            'editar'=>can2('editar-roles'),
+            'eliminar'=>can2('eliminar-roles')
+        ];
+        $notificaciones = Notificaciones::obtenerNotificaciones();
+        $cantidad = Notificaciones::obtenerCantidadNotificaciones();
         $datos = Usuarios::findOrFail(session()->get('Usuario_Id'));
         $roles = Roles::where('id', '<>', '6')->orderBy('id')->get();
-        return view('roles.listar', compact('roles', 'datos', 'notificaciones', 'cantidad', 'permisos'));
+
+        return view(
+            'roles.listar',
+            compact(
+                'roles',
+                'datos',
+                'notificaciones',
+                'cantidad',
+                'permisos'
+            )
+        );
     }
 
     /**
-     * Show the form for creating a new resource.
+     * Muestra el formulario para crear roles
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\View\View Vista del formulario de crear roles
      */
     public function crear()
     {
         can('crear-roles');
-        $notificaciones = Notificaciones::where('NTF_Para', '=', session()->get('Usuario_Id'))->orderByDesc('created_at')->get();
-        $cantidad = Notificaciones::where('NTF_Para', '=', session()->get('Usuario_Id'))->where('NTF_Estado', '=', 0)->count();
+        $notificaciones = Notificaciones::obtenerNotificaciones();
+        $cantidad = Notificaciones::obtenerCantidadNotificaciones();
         $datos = Usuarios::findOrFail(session()->get('Usuario_Id'));
-        return view('roles.crear', compact('datos', 'notificaciones', 'cantidad'));
+        
+        return view(
+            'roles.crear',
+            compact(
+                'datos',
+                'notificaciones',
+                'cantidad'
+            )
+        );
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Guarda los 
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param  App\Http\Requests\ValidacionRol  $request
+     * @return return redirect()->back()->with()
      */
     public function guardar(ValidacionRol $request)
     {
         $roles = Roles::where('RLS_Nombre_Rol', '=', $request->RLS_Nombre_Rol)
-            ->where('RLS_Empresa_Id', '=', session()->get('Empresa_Id'))->first();
-        if($roles){
-            return redirect()->back()->withErrors('Ya se encuentra registrado el rol en el sistema')->withInput();
+            ->where('RLS_Empresa_Id', '=', session()->get('Empresa_Id'))
+            ->first();
+        if ($roles) {
+            return redirect()
+                ->back()
+                ->withErrors('Ya se encuentra registrado el rol en el sistema')
+                ->withInput();
         }
         Roles::create([
             'RLS_Rol_Id' => 4,
@@ -61,67 +98,86 @@ class RolesController extends Controller
             'RLS_Descripcion_Rol' => $request->RLS_Descripcion_Rol,
             'RLS_Empresa_Id' => session()->get('Empresa_Id')
         ]);
+        
         return redirect()->back()->with('mensaje', 'Rol creado con exito');
     }
 
     /**
-     * Show the form for editing the specified resource.
+     * Muestra el formulario para editar un rol
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param  $id Identificador del rol
+     * @return \Illuminate\View\View Vista del formulario de editar roles
      */
     public function editar($id)
     {
         can('editar-roles');
-        $notificaciones = Notificaciones::where('NTF_Para', '=', session()->get('Usuario_Id'))->orderByDesc('created_at')->get();
-        $cantidad = Notificaciones::where('NTF_Para', '=', session()->get('Usuario_Id'))->where('NTF_Estado', '=', 0)->count();
+        $notificaciones = Notificaciones::obtenerNotificaciones();
+        $cantidad = Notificaciones::obtenerCantidadNotificaciones();
         $datos = Usuarios::findOrFail(session()->get('Usuario_Id'));
         $rol = Roles::findOrFail($id);
-        if($rol->RLS_Rol_Id != 4)
-            return redirect()->back()->withErrors(['El rol es por defecto del sistema, no es posible modificarlo.']);
-        return view('roles.editar', compact('rol', 'datos', 'notificaciones', 'cantidad'));
+        if ($rol->RLS_Rol_Id != 4) {
+            return redirect()
+                ->back()
+                ->withErrors(['El rol es por defecto del sistema, no es posible modificarlo.']);
+        }
+        return view(
+            'roles.editar',
+            compact(
+                'rol',
+                'datos',
+                'notificaciones',
+                'cantidad'
+            )
+        );
     }
 
     /**
-     * Update the specified resource in storage.
+     * Actualiza los datos del rol en la Base de Datos
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  App\Http\Requests\ValidacionRol  $request
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
     public function actualizar(ValidacionRol $request, $id)
     {
         $roles = Roles::where('RLS_Nombre_Rol', '<>', $request->RLS_Nombre_Rol)
-            ->where('RLS_Empresa_Id', '=', session()->get('Empresa_Id'))->get();
+            ->where('RLS_Empresa_Id', '=', session()->get('Empresa_Id'))
+            ->get();
         foreach ($roles as $rol) {
-            if($rol->RLS_Nombre_Rol==$request->RLS_Nombre_Rol){
-                return redirect()->back()->withErrors('Ya se encuentra registrado el rol en el sistema')->withInput();
+            if ($rol->RLS_Nombre_Rol==$request->RLS_Nombre_Rol) {
+                return redirect()
+                    ->back()
+                    ->withErrors('Ya se encuentra registrado el rol en el sistema')
+                    ->withInput();
             }
         }
         Roles::findOrFail($id)->update($request->all());
-        return redirect()->route('roles')->with('mensaje', 'Rol actualizado con exito');
+        return redirect()
+            ->route('roles')
+            ->with('mensaje', 'Rol actualizado con exito');
     }
 
     /**
-     * Remove the specified resource from storage.
+     * Elimina el rol seleccionado
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param  $id Identificador del rol
+     * @param  \Illuminate\Http\Request  $request
+     * @return response()->json()
      */
     public function eliminar(Request $request, $id)
     {
-        if(!can('eliminar-roles')){
+        if (!can('eliminar-roles')) {
             return response()->json(['mensaje' => 'np']);
-        }else{
-            if($request->ajax()){
+        } else {
+            if ($request->ajax()) {
                 $rol = Roles::findOrFail($id);
-                if($rol->RLS_Rol_Id != 4 || $rol->id == 4){
+                if ($rol->RLS_Rol_Id != 4 || $rol->id == 4) {
                     return response()->json(['mensaje' => 'rd']);
-                }else{
-                    try{
+                } else {
+                    try {
                         Roles::destroy($id);
                         return response()->json(['mensaje' => 'ok']);
-                    }catch(QueryException $e){
+                    } catch (QueryException $e) {
                         return response()->json(['mensaje' => 'ng']);
                     }
                 }
