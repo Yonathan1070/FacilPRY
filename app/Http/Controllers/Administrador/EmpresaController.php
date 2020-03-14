35f714af13b8f86a1b87fc3240943e3dfa2c1a3f
Yonathan Bohorquez <ycbohorquez@ucundinamarca.edu.co>
2020-03-14 08:42:17 -0500

    Librerias innecesarias

--- app/Http/Controllers/Administrador/EmpresaController.php
+++ app/Http/Controllers/Administrador/EmpresaController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Administrador;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 use App\Models\Tablas\Empresas;
 use App\Models\Tablas\Notificaciones;
