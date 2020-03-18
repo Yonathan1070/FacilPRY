565aac9f91551f275233064a91a7847c11481b16
Yonathan Bohorquez <ycbohorquez@ucundinamarca.edu.co>
2020-03-17 19:05:15 -0500

    Código documentado perfil usuario

--- app/Http/Controllers/PerfilUsuarioController.php
+++ app/Http/Controllers/PerfilUsuarioController.php
@@ -6,70 +6,58 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Tablas\Usuarios;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\Facades\File;
-use App\Http\Requests\ValidacionUsuario;
 use App\Models\Tablas\Notificaciones;
 
+/**
+ * Perfil Usuario Controller, donde se mostrarán los datos
+ * del usuario autenticado y se realizaran las distintas
+ * actualizaciones del perfil
+ * 
+ * @author: Yonathan Bohorquez
+ * @email: ycbohorquez@ucundinamarca.edu.co
+ * 
+ * @author: Manuel Bohorquez
+ * @email: jmbohorquez@ucundinamarca.edu.co
+ * 
+ * @version: dd/MM/yyyy 1.0
+ */
 class PerfilUsuarioController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Muestra la vista para la edición del perfil de usuario
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\View\View Vista del perfil de usuario
      */
     public function index()
     {
         can('editar-perfil');
-        $notificaciones = Notificaciones::where('NTF_Para', '=', session()->get('Usuario_Id'))->orderByDesc('created_at')->get();
-        $cantidad = Notificaciones::where('NTF_Para', '=', session()->get('Usuario_Id'))->where('NTF_Estado', '=', 0)->count();
+
+        $notificaciones = Notificaciones::obtenerNotificaciones();
+        $cantidad = Notificaciones::obtenerCantidadNotificaciones();
         $datos = Usuarios::findOrFail(session()->get('Usuario_Id'));
-        return view('perfil.editar', compact('datos', 'notificaciones', 'cantidad'));
+        
+        return view(
+            'perfil.editar',
+            compact(
+                'datos',
+                'notificaciones',
+                'cantidad'
+            )
+        );
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
+     * Actualiza la foto de perfil del usuario
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return response()->json()
      */
     public function actualizarFoto(Request $request)
     {
         $validator = Validator::make($request->all(), [
-            'USR_Foto_Perfil_Usuario' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'USR_Foto_Perfil_Usuario' =>
+                'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
         ]);
 
         if ($validator->passes()) {
@@ -79,56 +67,81 @@ class PerfilUsuarioController extends Controller
                 unlink($ruta);
             }
             $input = $request->all();
-            $nombreArchivo = $input['USR_Foto_Perfil_Usuario'] = time() . '.' . $request->USR_Foto_Perfil_Usuario->getClientOriginalExtension();
-            $request->USR_Foto_Perfil_Usuario->move(public_path('assets/bsb/images'), $input['USR_Foto_Perfil_Usuario']);
-            Usuarios::findOrFail(session()->get('Usuario_Id'))->update(['USR_Foto_Perfil_Usuario' => $nombreArchivo]);
-            return response()->json(['success' => 'Foto Actualizada']);
+            $nombreArchivo = $input['USR_Foto_Perfil_Usuario'] = time().
+                '.'.
+                $request->USR_Foto_Perfil_Usuario->getClientOriginalExtension();
+            $request->USR_Foto_Perfil_Usuario->move(
+                public_path('assets/bsb/images'),
+                $input['USR_Foto_Perfil_Usuario']
+            );
+            Usuarios::findOrFail(session()->get('Usuario_Id'))
+                ->update(['USR_Foto_Perfil_Usuario' => $nombreArchivo]);
+            
+            return response()
+                ->json(['success' => 'Foto Actualizada']);
         }
-        return response()->json(['error' => $validator->errors()->all()]);
+
+        return response()
+            ->json(['error' => $validator->errors()->all()]);
     }
 
     /**
-     * Update the specified resource in storage.
+     * Actualiza los datos del perfil del usuario autenticado
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return redirect()->back()->with()
      */
     public function actualizarDatos(Request $request)
     {
         $usuarios = Usuarios::where('USR_Empresa_Id', '=', session()->get('Empresa_Id'))
-            ->where('id', '<>', session()->get('Usuario_Id'))->get();
+            ->where('id', '<>', session()->get('Usuario_Id'))
+            ->get();
         foreach ($usuarios as $usuario) {
             if($usuario->USR_Documento_Usuario == $request->USR_Documento_Usuario){
-                return redirect()->back()->withErrors('El Documento ya se encuentra en uso.');
+                return redirect()
+                    ->back()
+                    ->withErrors('El Documento ya se encuentra en uso.');
             }
             if($usuario->USR_Correo_Usuario == $request->USR_Correo_Usuario){
-                return redirect()->back()->withErrors('El correo electrónico ya se encuentra en uso.');
+                return redirect()
+                    ->back()
+                    ->withErrors('El correo electrónico ya se encuentra en uso.');
             }
         }
-        Usuarios::findOrFail(session()->get('Usuario_Id'))->update($request->all());
-        return redirect()->back()->with('mensaje', 'Datos actualizados con exito');
+        Usuarios::findOrFail(session()->get('Usuario_Id'))
+            ->update($request->all());
+        
+        return redirect()
+            ->back()
+            ->with('mensaje', 'Datos actualizados con exito');
     }
 
     /**
-     * Remove the specified resource from storage.
+     * Actualiza la contraseña del usuario
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param  \Illuminate\Http\Request  $request
+     * @return redirect()->back()->with()
      */
     public function actualizarClave(Request $request)
     {
         if($request->USR_Clave_Nueva != $request->USR_Clave_Confirmar){
             return redirect()->back()->withErrors('Las contraseñas no coinciden.');
         }
-        $clave = Usuarios::select('password')->where('id', '=', session()->get('Usuario_Id'))->first();
+        $clave = Usuarios::select('password')
+            ->where('id', '=', session()->get('Usuario_Id'))
+            ->first();
         $correcta = Hash::check($request->USR_Clave_Anterior, $clave->password, []);
         if (!$correcta) {
-            return redirect()->back()->withErrors('La contraseña antigua es incorrecta.');
+            return redirect()
+                ->back()
+                ->withErrors('La contraseña antigua es incorrecta.');
         }
         Usuarios::findOrFail(session()->get('Usuario_Id'))->update([
             'password' => bcrypt($request->USR_Clave_Nueva)
         ]);
-        return redirect()->back()->with('mensaje', 'Contraseña actualizada.');
+        
+        return redirect()
+            ->back()
+            ->with('mensaje', 'Contraseña actualizada.');
     }
 }
