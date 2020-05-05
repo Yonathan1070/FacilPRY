573ad4adb8a181502bdf33d9da0a1b4594cbce72
Yonathan Bohorquez <ycbohorquez@ucundinamarca.edu.co>
2020-05-05 15:40:27 -0500

    Adición de unos datos, solicitado por ALEJANDRO

--- resources/views/director/perfiloperacion/carga/actividades.blade.php
+++ resources/views/director/perfiloperacion/carga/actividades.blade.php
@@ -42,7 +42,12 @@
                                 <tbody>
                                     @foreach ($actividades as $actividad)
                                     <tr>
-                                        <td>{{$actividad->ACT_Nombre_Actividad}}</td>
+                                        <td>
+                                            <a id="{{$actividad->Id_Actividad}}" onclick="detalleActividad(this)" class="btn-accion-tabla tooltipsC"
+                                                title="Ver Detalles" data-toggle="modal" data-target="#defaultModal">
+                                                {{$actividad->ACT_Nombre_Actividad}}
+                                            </a>
+                                        </td>
                                         <td>{{$actividad->ACT_Descripcion_Actividad}}</td>
                                         <td>{{$actividad->REQ_Nombre_Requerimiento}}</td>
                                         <td>{{$actividad->ACT_Fecha_Fin_Actividad}}</td>
@@ -150,6 +155,105 @@
                         </div>
                     </li>
                 </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="modal fade" id="defaultModal" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title" id="defaultModalLabel">Detalles de la tarea</h4>
+            </div>
+            <div class="modal-body">
+                <div class="row clearfix">
+                    <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
+                        <label for="nombreActividadDetalle">Tarea</label>
+                    </div>
+                    <div class="col-lg-10 col-md-10 col-sm-8 col-xs-7">
+                        <div class="form-group">
+                            <div class="form-line">
+                                <input type="text" id="nombreActividadDetalle" class="form-control" readonly="true">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row clearfix">
+                    <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
+                        <label for="descripcionActividadDetalle">Descripción</label>
+                    </div>
+                    <div class="col-lg-10 col-md-10 col-sm-8 col-xs-7">
+                        <div class="form-group">
+                            <div class="form-line">
+                                <textarea name="" id="descripcionActividadDetalle" readonly="true" class="form-control"></textarea>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row clearfix">
+                    <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
+                        <label for="nombreRequerimientoDetalle">Actividad</label>
+                    </div>
+                    <div class="col-lg-10 col-md-10 col-sm-8 col-xs-7">
+                        <div class="form-group">
+                            <div class="form-line">
+                                <input type="text" id="nombreRequerimientoDetalle" class="form-control" readonly="true">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row clearfix">
+                    <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
+                        <label for="nombreProyectoDetalle">Proyecto</label>
+                    </div>
+                    <div class="col-lg-10 col-md-10 col-sm-8 col-xs-7">
+                        <div class="form-group">
+                            <div class="form-line">
+                                <input type="text" id="nombreProyectoDetalle" class="form-control" readonly="true">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row clearfix">
+                    <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
+                        <label for="nombreEmpresaDetalle">Empresa</label>
+                    </div>
+                    <div class="col-lg-10 col-md-10 col-sm-8 col-xs-7">
+                        <div class="form-group">
+                            <div class="form-line">
+                                <input type="text" id="nombreEmpresaDetalle" class="form-control" readonly="true">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row clearfix">
+                    <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
+                        <label for="fechaInicioActividadDetalle">Inicio</label>
+                    </div>
+                    <div class="col-lg-10 col-md-10 col-sm-8 col-xs-7">
+                        <div class="form-group">
+                            <div class="form-line">
+                                <input type="text" id="fechaInicioActividadDetalle" class="form-control" readonly="true">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row clearfix">
+                    <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
+                        <label for="fechaFinActividadDetalle">Fin</label>
+                    </div>
+                    <div class="col-lg-10 col-md-10 col-sm-8 col-xs-7">
+                        <div class="form-group">
+                            <div class="form-line">
+                                <input type="text" id="fechaFinActividadDetalle" class="form-control" readonly="true">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <input type="hidden" name="idActividad" id="idActividad" class="form-control" readonly="true">
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-link waves-effect" data-dismiss="modal">Cerrar</button>
+                </div>
             </div>
         </div>
     </div>
